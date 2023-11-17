49028ea43c6bf0f6099f234cd0b5b0fb305c4d7e
Your <ckiroshan@gmail.com>
2023-11-17 15:16:13 +0530

    Added ID incrementation for each new job created.

--- src/main/java/com/irocodes/firstjobapp/job/JobServiceImpl.java
+++ src/main/java/com/irocodes/firstjobapp/job/JobServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class JobServiceImpl implements JobService {
     // Temporary Array List [As DB is not yet connected]
     private List<Job> jobs = new ArrayList<>();
+    private Long nextId = 1L; // ID variable
 
     @Override
     public List<Job> findAll() {
@@ -17,6 +18,7 @@ public class JobServiceImpl implements JobService {
 
     @Override
     public void createJob(Job job) {
+        job.setId(nextId++); // Each save, value increments by 1.
         jobs.add(job);
     }
 }
