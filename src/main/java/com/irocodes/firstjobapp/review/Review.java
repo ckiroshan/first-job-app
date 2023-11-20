48446294669e36a59ac10d6a798acb2b50c2a32b
Your <ckiroshan@gmail.com>
2023-11-20 14:59:19 +0530

    Configured the Review entity class.

--- src/main/java/com/irocodes/firstjobapp/review/Review.java
+++ src/main/java/com/irocodes/firstjobapp/review/Review.java
@@ -1,4 +1,59 @@
 package com.irocodes.firstjobapp.review;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private double rating;
+
+
+    // Constructors
+    public Review() {
+    }
+
+    public Review(Long id, String title, String description, double rating) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
 }
